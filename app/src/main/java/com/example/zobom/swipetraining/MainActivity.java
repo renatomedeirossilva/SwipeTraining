96233f3f267443c1143c4d15e68a4f7b4f1c6dbe
renatomedeirossilva <proj.renatosilva@gmail.com>
2018-10-19 20:59:10 +0000

    Initial commit

---
+++ app/src/main/java/com/example/zobom/swipetraining/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.zobom.swipetraining;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+public class MainActivity extends FragmentActivity {
+    ViewPager viewpager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        viewpager = (ViewPager) findViewById(R.id.pager);
+        PagerAdapter padapter = new PagerAdapter(getSupportFragmentManager());
+        viewpager.setAdapter(padapter);
+    }
+}
---
+++ app/src/main/java/com/example/zobom/swipetraining/PagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.zobom.swipetraining;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        // TODO Auto-generated method stub
+        switch (arg0) {
+            case 0:
+                return new FragmentOne();
+            case 1:
+                return new FragmentTwo();
+            case 2:
+                return new FragmentThree();
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return 3;
+    }
+
+
+}
